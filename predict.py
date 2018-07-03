747b72576b3e6e263dfef659dde593b90728052e
Grant <grant.schoffelen@gmail.com>
2018-07-03 00:33:01 -0400

    train, and predict init

--- predict.py
+++ predict.py
@@ -1,1 +1,23 @@
 
+import argparse
+from os import listdir
+
+
+def main():
+    in_arg = get_input_args()
+
+def get_input_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--top_k', type=int, default=3,
+                        help='return top k most likely classes')
+    parser.add_argument('--category_names', type=str, default='cat_to_name.json',
+                        help='architecture model')
+    parser.add_argument('--gpu', type=int, action='store_true',
+                        help='use gpu')
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
--- train.py
+++ train.py
@@ -1,1 +1,30 @@
+import argparse
+from os import listdir
 
+
+def main():
+    in_arg = get_input_args()
+
+def get_input_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--save_dir', type=str, default='checkpoints/',
+                        help='directory to save checkpoints')
+    parser.add_argument('--arch', type=str, default='vgg',
+                        help='architecture model')
+    parser.add_argument('--learning_rate', type=float, default=0.01,
+                        help='learning rate')
+    parser.add_argument('--hidden_units', type=int, default=512,
+                        help='hidden units')
+    parser.add_argument('--epochs', type=int, default=3,
+                        help='epochs')
+    parser.add_argument('--epochs', type=int, default=3,
+                        help='epochs')
+    parser.add_argument('--gpu', type=int, action='store_true',
+                        help='use gpu')
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
